26c66268e225da03a567b1432e00581e895d45ad
mgrigoreva@phoenixit.ru <mgrigoreva@phoenixit.ru>
2021-08-03 23:13:54 +0300

    Занятие 2: Объекты. Все задачи

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,9 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  for (let key in salaries) {
+    if (salaries[key] && salaries[key] !== Infinity && salaries[key] !== -Infinity && typeof(salaries[key]) === 'number') {
+      sum = sum + salaries[key];
+    }
+  }
+  return sum;
 }
