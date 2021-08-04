3e26d07504414418873224815eea83d8bbc349d7
mgrigoreva@phoenixit.ru <mgrigoreva@phoenixit.ru>
2021-08-04 11:40:42 +0300

    Занятие 3: Массивы. Все задачи

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,13 @@
 function showSalary(users, age) {
-  // ваш код...
+  let filtredUsers;
+  filtredUsers = users.filter(i => i.age <= age);
+  let result = '';
+
+  filtredUsers.forEach((element, index) => {
+    result = result + `${element.name}, ${element.balance}`
+    if (index + 1 < filtredUsers.length) {
+      result = result + '\n'
+    }
+  });
+  return result
 }
