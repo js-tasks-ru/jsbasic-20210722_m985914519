30b1381adfd914c8e5699a4f046299ef6a2d905b
mgrigoreva@phoenixit.ru <mgrigoreva@phoenixit.ru>
2021-08-16 18:57:38 +0300

    решение задач Занятие 4: Знакомство с DOM

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,14 @@
 function highlight(table) {
-  // ваш код...
+  for (let i = 1; i < table.rows.length; i++) {
+    let status = table.rows[i].cells[3].dataset.available;
+    String(status) === 'true' ? table.rows[i].classList.add('available') :
+      String(status) === 'false' ? table.rows[i].classList.add('unavailable') :
+        table.rows[i].setAttribute('hidden', true);
+    let gender = table.rows[i].cells[2].innerHTML;
+    table.rows[i].classList.add(gender === 'm' ? 'male' : 'female');
+    let age = table.rows[i].cells[1].innerHTML;
+    if (age < 18) {
+      table.rows[i].style.textDecoration = 'line-through';
+    }
+  }
 }
