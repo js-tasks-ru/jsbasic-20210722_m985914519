0d374f91944643540c7cce84a6408cca5e2edaa3
mgrigoreva@phoenixit.ru <mgrigoreva@phoenixit.ru>
2021-09-07 19:07:29 +0300

    Модуль 7 все задачи

--- 7-module/4-task/index.js
+++ 7-module/4-task/index.js
@@ -1,18 +1,19 @@
 export default class StepSlider {
   constructor({ steps, value = 0 }) {
-    this.config = { steps : steps, value : 0 }
+    this.config = { steps: steps, value: 0 }
     this.render();
     this.thumb = this.elem.querySelector('.slider__thumb');
+    this.progress = this.elem.querySelector('.slider__progress');
     this.thumb.ondragstart = () => false;
     this.thumb.pointerdown = () => false;
     this.thumb.pointermove = () => false;
-    this.thumb.addEventListener('pointerdown', this.dragSlider)
-    this.thumb.addEventListener('pointerup', this.dropSlider)
+    this.thumb.addEventListener('pointerdown', this.dragSlider);
+    this.elem.addEventListener('click', this.changeSlider);
   }
 
   render() {
     this.sliderSteps = `<span class="slider__step-active"></span>`;
-    for(let i = 1; i < this.config.steps; i++) {
+    for (let i = 1; i < this.config.steps; i++) {
       this.sliderSteps = this.sliderSteps + `<span></span>`
     }
     this.elem = document.createElement('div')
@@ -32,8 +33,8 @@ export default class StepSlider {
 
   changeSlider = (event) => {
     let sliderСoordinateX = event.clientX - this.elem.getBoundingClientRect().left
-    let oneStep = this.elem.offsetWidth/ (this.config.steps - 1)
-    let numberStep = Math.round(sliderСoordinateX/ oneStep)
+    let oneStep = this.elem.offsetWidth / (this.config.steps - 1)
+    let numberStep = Math.round(sliderСoordinateX / oneStep)
     let spans = this.elem.querySelector('.slider__steps').querySelectorAll('span')
     spans[this.config.value].classList.remove('slider__step-active')
     this.config.value = numberStep
@@ -41,13 +42,10 @@ export default class StepSlider {
     let thumb = this.elem.querySelector('.slider__thumb');
     let progress = this.elem.querySelector('.slider__progress');
 
-    let leftPercents = 100/(this.config.steps-1) * numberStep;
+    let leftPercents = 100 / (this.config.steps - 1) * numberStep;
     thumb.style.left = `${leftPercents}%`;
     progress.style.width = `${leftPercents}%`;
     this.elem.querySelector('.slider__value').textContent = `${numberStep}`
-
-
-    this.elem.classList.add('slider_dragging')
 
     let customEvent = new CustomEvent('slider-change', {
       detail: this.config.value,
@@ -57,35 +55,52 @@ export default class StepSlider {
   }
 
   dragSlider = (event) => {
+    event.preventDefault();
+    this.elem.classList.add('slider_dragging')
     this.thumb.style.position = 'absolute';
     this.thumb.style.zIndex = 1000;
-
-    this.elem.classList.add('slider_dragging')
-console.log(this.thumb)
     this.move(event);
-    
-      // centers the ball at (pageX, pageY) coordinates
-
-      document.addEventListener('pointermove', move);
-  /*  let customEvent = new CustomEvent('slider-change', {
-      detail: this.config.value,
-      bubbles: true
-    })
-    this.elem.dispatchEvent(customEvent); */
+    document.addEventListener('pointermove', this.move);
+    document.addEventListener('pointerup', this.dropSlider);
   }
-  move = (event) => {
-    console.log(this.thumb)
-    let { pageX, pageY } = event;
-    let sliderHalfWidth = this.thumb.offsetWidth / 2;
-    let sliderHalfHeight = this.thumb.offsetHeight / 2;
 
-    this.thumb.style.left = `${pageX - sliderHalfWidth}px`;
-    this.thumb.style.top = `${pageY - sliderHalfHeight}px`;
+  move = (event) => {
+    let sliderСoordinateX = event.clientX - this.elem.getBoundingClientRect().left
+    if (sliderСoordinateX < 0 || sliderСoordinateX > this.elem.getBoundingClientRect().width) {
+      return;
+    }
+    let leftRelative = sliderСoordinateX / this.elem.offsetWidth;
+    let leftPercents = leftRelative * 100;
+    this.thumb.style.left = `${leftPercents}%`;
+    this.progress.style.width = `${leftPercents}%`;
+
+    let oneStep = this.elem.offsetWidth / (this.config.steps - 1)
+    let numberStep = Math.round(sliderСoordinateX / oneStep)
+    this.elem.querySelector('.slider__value').textContent = `${numberStep}`
   }
 
   dropSlider = (event) => {
     this.elem.classList.remove('slider_dragging')
-    document.removeEventListener('pointermove', move);
+    document.removeEventListener('pointermove', this.move);
     document.onpointerup = null;
+
+    let sliderСoordinateX = event.clientX - this.elem.getBoundingClientRect().left
+    let oneStep = this.elem.offsetWidth / (this.config.steps - 1)
+    let numberStep = Math.round(sliderСoordinateX / oneStep)
+    let spans = this.elem.querySelector('.slider__steps').querySelectorAll('span')
+    spans[this.config.value].classList.remove('slider__step-active')
+    this.config.value = numberStep
+    spans[this.config.value].classList.add('slider__step-active')
+
+    let leftPercents = 100 / (this.config.steps - 1) * numberStep;
+    this.thumb.style.left = `${leftPercents}%`;
+    this.progress.style.width = `${leftPercents}%`;
+    this.elem.querySelector('.slider__value').textContent = `${numberStep}`
+
+    let customEvent = new CustomEvent('slider-change', {
+      detail: this.config.value,
+      bubbles: true
+    })
+    this.elem.dispatchEvent(customEvent);
   }
 }
